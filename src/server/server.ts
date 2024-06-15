c8d1113cad7cdb58206d8ea581219b5f3ae10dd6
FeIronMonkey <senderesnick@gmail.com>
2024-06-15 02:51:39 -0400

    added test explorer, got unit tests debuging working with attached debugger, reorganized project a bit and properly copied non-ts files to the dist directory after transpile

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,10 +8,9 @@ const port: number = 3000;
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join('/scripts', '../public/scripts')));
 
 app.get('/', (request: Request, response: Response) => {
-  response.send('Hello, Typescript.... demo text!');
+  response.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(port, () => {
--- test/shared/utils/add.test.ts
+++ test/shared/utils/add.test.ts
@@ -1,12 +1,16 @@
-import { assert } from 'console';
-import { add } from '../../src/utils/add';
+import { add } from '../../../src/shared/utils/add';
 import { expect } from 'chai';
 
 describe('add function', () => {
   it('should add two numbers correctly', () => {
     const result = add(2, 3);
 
-    assert(result === 5);
     expect(result).to.equal(5);
+  });
+
+  it('should add 2 and 2 correctly', () => {
+    const result = add(2, 2);
+
+    expect(result).to.equal(4);
   });
 });
