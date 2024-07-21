7980ef0ee15b1d6160d22d4bd41eff77d8441bae
FeIronMonkey <senderesnick@gmail.com>
2024-07-21 06:31:29 -0400

    added logic for other operators to html page script, but test still failing to recognize add.test.ts extension for unknown reason

--- src/public/math/operations/script.ts
+++ src/public/math/operations/script.ts
@@ -1,4 +1,12 @@
-import { add, subtract } from '../../shared/utils/mathFunctions';
+import {
+  add,
+  subtract,
+  multiply,
+  divide,
+  isLessThan,
+  isGreaterThan,
+  areEqual,
+} from '../../shared/utils/mathFunctions.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   addEventListeners();
@@ -7,10 +15,10 @@ document.addEventListener('DOMContentLoaded', () => {
 function addEventListeners() {
   const addButton = document.getElementById('add-button');
   if (addButton) {
-    addButton.onclick = (_: MouseEvent) => {
-      const input1 = document.getElementById('add-element-1') as HTMLInputElement;
+    addButton.onclick = () => {
+      const input1 = document.getElementById('add-input-1') as HTMLInputElement;
       const a = Number(input1?.value);
-      const input2 = document.getElementById('add-element-1') as HTMLInputElement;
+      const input2 = document.getElementById('add-input-2') as HTMLInputElement;
       const b = Number(input2?.value);
 
       const output = document.getElementById('add-output') as HTMLInputElement;
@@ -20,10 +28,10 @@ function addEventListeners() {
 
   const subtractButton = document.getElementById('subtract-button');
   if (subtractButton) {
-    subtractButton.onclick = (_: MouseEvent) => {
-      const input1 = document.getElementById('subtract-element-1') as HTMLInputElement;
+    subtractButton.onclick = () => {
+      const input1 = document.getElementById('subtract-input-1') as HTMLInputElement;
       const a = Number(input1?.value);
-      const input2 = document.getElementById('subtract-element-1') as HTMLInputElement;
+      const input2 = document.getElementById('subtract-input-2') as HTMLInputElement;
       const b = Number(input2?.value);
 
       const output = document.getElementById('subtract-output') as HTMLInputElement;
@@ -33,36 +41,66 @@ function addEventListeners() {
 
   const multiplyButton = document.getElementById('multiply-button');
   if (multiplyButton) {
-    multiplyButton.onclick = (event: MouseEvent) => {
-      console.log(event);
+    multiplyButton.onclick = () => {
+      const input1 = document.getElementById('multiply-input-1') as HTMLInputElement;
+      const a = Number(input1?.value);
+      const input2 = document.getElementById('multiply-input-2') as HTMLInputElement;
+      const b = Number(input2?.value);
+
+      const output = document.getElementById('multiply-output') as HTMLInputElement;
+      output!.value = multiply(a, b).toString();
     };
   }
 
   const divideButton = document.getElementById('divide-button');
   if (divideButton) {
-    divideButton.onclick = (event: MouseEvent) => {
-      console.log(event);
+    divideButton.onclick = () => {
+      const input1 = document.getElementById('divide-input-1') as HTMLInputElement;
+      const a = Number(input1?.value);
+      const input2 = document.getElementById('divide-input-2') as HTMLInputElement;
+      const b = Number(input2?.value);
+
+      const output = document.getElementById('divide-output') as HTMLInputElement;
+      output!.value = divide(a, b).toString();
     };
   }
 
   const gtButton = document.getElementById('gt-button');
   if (gtButton) {
-    gtButton.onclick = (event: MouseEvent) => {
-      console.log(event);
+    gtButton.onclick = () => {
+      const input1 = document.getElementById('gt-input-1') as HTMLInputElement;
+      const a = Number(input1?.value);
+      const input2 = document.getElementById('gt-input-2') as HTMLInputElement;
+      const b = Number(input2?.value);
+
+      const output = document.getElementById('gt-output') as HTMLInputElement;
+      output!.value = isGreaterThan(a, b) ? 'Yes' : 'No';
     };
   }
 
   const ltButton = document.getElementById('lt-button');
   if (ltButton) {
-    ltButton.onclick = (event: MouseEvent) => {
-      console.log(event);
+    ltButton.onclick = () => {
+      const input1 = document.getElementById('lt-input-1') as HTMLInputElement;
+      const a = Number(input1?.value);
+      const input2 = document.getElementById('lt-input-2') as HTMLInputElement;
+      const b = Number(input2?.value);
+
+      const output = document.getElementById('lt-output') as HTMLInputElement;
+      output!.value = isLessThan(a, b) ? 'Yes' : 'No';
     };
   }
 
   const eqButton = document.getElementById('eq-button');
   if (eqButton) {
-    eqButton.onclick = (event: MouseEvent) => {
-      console.log(event);
+    eqButton.onclick = () => {
+      const input1 = document.getElementById('eq-input-1') as HTMLInputElement;
+      const a = Number(input1?.value);
+      const input2 = document.getElementById('eq-input-2') as HTMLInputElement;
+      const b = Number(input2?.value);
+
+      const output = document.getElementById('eq-output') as HTMLInputElement;
+      output!.value = areEqual(a, b) ? 'Yes' : 'No';
     };
   }
 }
--- src/public/shared/utils/mathFunctions.ts
+++ src/public/shared/utils/mathFunctions.ts
@@ -6,4 +6,24 @@ function subtract(a: number, b: number): number {
   return a - b;
 }
 
-export { add, subtract };
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function isLessThan(a: number, b: number): boolean {
+  return a < b;
+}
+
+function isGreaterThan(a: number, b: number): boolean {
+  return a > b;
+}
+
+function areEqual(a: number, b: number): boolean {
+  return a === b;
+}
+
+export { add, subtract, multiply, divide, isLessThan, isGreaterThan, areEqual };
